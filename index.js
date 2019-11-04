011e3dfdd886848b2cc4e45a27b30a40956ec994
Nullbit46 <finneganreynolds@gmail.com>
2019-11-04 09:00:51 -0600

    Added ?monday command to build an automated event message later
    *Added ?monday case
    
    *Added "monday" to list of commands

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ const Prefix = '?';
 
 const Prefix2 = "It's";
 
-const baseCommands=['signup','list','register','clear','interested','uninterested','connect','disconnect','sendnewts'];
+const baseCommands=['signup','list','register','clear','interested','uninterested','connect','disconnect','sendnewts',"monday"];
 const commands=baseCommands.map(function(command){return Prefix.concat(command)});
 
 require('log-timestamp');
@@ -167,6 +167,13 @@ Bot.on('ready', () => {
             
             console.log("the lizard number was " + pet);
         break;
+        case '?monday' :
+        msg.guild.members.fetch;  
+        Bot.channels.get("479411609329074176").send("@here COME ONE COME ALL COME ON DOWN FOR A MONDAY RAID! https://forms.gle/iBrpCWBNsdmm3PNK7");
+        
+        console.log(msg.author.username + ' sent the monday message');
+              
+      	break;
     };
 
    });
